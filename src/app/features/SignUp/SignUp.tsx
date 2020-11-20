58a1abe3a683bc626fe31e03dd0c3d1856d59986
pchmn <pchmn.dev@gmail.com>
2020-11-20 23:24:15 +0100

    Rework signup (only 1 step)

--- src/app/features/SignUp/SignUp.tsx
+++ src/app/features/SignUp/SignUp.tsx
@@ -1,50 +1,84 @@
+import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useFormik } from 'formik';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { useSelector } from 'react-redux';
-import { EmailStep } from './EmailStep/EmailStep';
-import { PasswordStep } from './PasswordStep/PasswordStep';
+import { useDispatch } from 'react-redux';
+import * as yup from 'yup';
 import styles from './SignUp.module.scss';
-import { selectActiveStep } from './signUpSlice';
 
 
 export function SignUp() {
-  const activeStep = useSelector(selectActiveStep);
+  const dispatch = useDispatch();
+  const matches = useMediaQuery('(max-width: 530px)');
   const { t } = useTranslation();
 
-  // console.log(frTranslations);
+  const validationSchema = yup.object({
+    email: yup
+      .string()
+      .email(t('common.errors.invalidField', { field: t('signUp.email') }))
+      .required(t('common.errors.requiredField')),
+    username: yup
+      .string()
+      .required(t('common.errors.requiredField')),
+    password: yup
+      .string()
+      .min(8, t('common.errors.minLength', { min: 8 }))
+      .required(t('common.errors.requiredField')),
+    confirmPassword: yup
+      .string()
+      .oneOf([yup.ref('password'), ''], t('signUp.passwordsMismatch'))
+      .required(t('common.errors.requiredField')),
+  });
 
-  const transitionOptions = {
-    transitionName: "fade",
-    transitionEnterTimeout: 500,
-    transitionLeaveTimeout: 500
-  }
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      username: '',
+      password: '',
+      confirmPassword: ''
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
 
-  const getStep = () => {
-    switch (activeStep) {
-      case 0: return <EmailStep key="0" />;
-      case 1: return <PasswordStep key="1" />
-      default: return <EmailStep key="2" />
-    }
+  function textField(field: 'email' | 'username' | 'password' | 'confirmPassword') {
+    return <TextField
+      className={styles.textField}
+      fullWidth
+      size="small"
+      variant="outlined"
+      id={field}
+      name={field}
+      type={['password', 'confirmPassord'].includes(field) ? 'password' : 'text'}
+      label={t(`signUp.${field}`)}
+      value={formik.values[field]}
+      onChange={formik.handleChange}
+      error={formik.touched[field] && Boolean(formik.errors[field])}
+      helperText={formik.touched[field] && formik.errors[field]}
+    />;
   }
 
   return (
-    <Container maxWidth="md">
-      <Paper>
-        <Grid container className={styles.SignUp} direction={'row'} spacing={0}>
-
-          <Grid item xs={6} className={styles.leftPart}>
-            <div><img alt="lol" src="./images/logo-48px.png" /></div>
-            <h2>{t('signUp.title')}</h2>
-          </Grid>
-
-          <Grid item xs={6} className={styles.rightPart}>
-            {getStep()}
-          </Grid>
-
-        </Grid>
+    <Container className={`${styles.SignUp} ${matches ? 'flex-column' : ''}`} maxWidth="sm">
+      <div><img className={styles.logo} alt="logo" src="./images/logo-48px.png" /></div>
+      <Paper className={`${styles.paper} darker`}>
+        <h2 className={styles.title}>{t('signUp.title')}</h2>
+        <form className={styles.form} onSubmit={formik.handleSubmit}>
+          {textField('email')}
+          {textField('username')}
+          <span className={styles.passwordHelper}>{t('signUp.passwordHelper')}</span>
+          {textField('password')}
+          {textField('confirmPassword')}
+          <Button className={styles.submitButton} variant="contained" color="primary" type="submit">
+            {t('signUp.validate')}
+          </Button>
+        </form>
       </Paper>
     </Container>
   );
