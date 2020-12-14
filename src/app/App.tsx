6d3bfdf97f8c29e06f2a2a55be81da67a25058b1
pchmn <pchmn.dev@gmail.com>
2020-12-14 02:29:05 +0100

    Create Home component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
-import { BrowserRouter, Redirect } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 import './App.scss';
 import { withInitApp } from './core/initApp/withInitApp';
 import { Auth } from './features/Auth/Auth';
+import Home from './features/Home/Home';
 import { AnimatedRoute, AnimatedSwitch } from './shared/animation/RouteTransition';
 
 
 function AppComponent() {
   return (
-    <BrowserRouter>
-      <AnimatedSwitch>
-        <AnimatedRoute path="/(signin|signup)" fullHeight={true}>
-          <Auth />
-        </AnimatedRoute>
-        <AnimatedRoute exact path="/">
-          <Redirect to="/signup/account-created" />
-        </AnimatedRoute>
-      </AnimatedSwitch>
-    </BrowserRouter>
+    <AnimatedSwitch>
+      <AnimatedRoute path="/(signin|signup)" fullHeight={true}>
+        <Auth />
+      </AnimatedRoute>
+      <AnimatedRoute path="/my-account" fullHeight={true}>
+        <Home />
+      </AnimatedRoute>
+      <AnimatedRoute exact path="/">
+        <Redirect to="/my-account" />
+      </AnimatedRoute>
+    </AnimatedSwitch>
   );
 }
 
---
+++ src/app/features/Home/Home.tsx
@@ -0,0 +1,201 @@
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Drawer from '@material-ui/core/Drawer';
+import IconButton from '@material-ui/core/IconButton';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined';
+import ChevronRightOutlinedIcon from '@material-ui/icons/ChevronRightOutlined';
+import SettingsOutlinedIcon from '@material-ui/icons/SettingsOutlined';
+import clsx from 'clsx';
+import React, { useEffect, useState } from 'react';
+import styles from './Home.module.scss';
+
+const drawerWidth = 290;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+  },
+  drawerOpen: {
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerClose: {
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: theme.spacing(9) + 1,
+    [theme.breakpoints.up('sm')]: {
+      width: '85px',
+    },
+  },
+  toolbar: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+}));
+
+export default function Home() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+  let [textVisibility, setTextVisibility] = useState<'hidden' | 'visible'>();
+
+  useEffect(() => {
+    setTextVisibility('hidden');
+  }, [])
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+    setTextVisibility('visible');
+    console.log(textVisibility)
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+    setTimeout(() => setTextVisibility('hidden'), 225)
+  };
+
+  function toggleDrawer() {
+    setOpen(!open);
+  }
+
+  return (
+    <div className={`${classes.root} ${styles.Home}`}>
+      <CssBaseline />
+      {/* <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, {
+              [classes.hide]: open,
+            })}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Mini variant drawer
+          </Typography>
+        </Toolbar>
+      </AppBar> */}
+      <Drawer
+        variant="permanent"
+        className={clsx(classes.drawer, {
+          [classes.drawerOpen]: open,
+          [classes.drawerClose]: !open,
+        })}
+        classes={{
+          paper: clsx({
+            [classes.drawerOpen]: open,
+            [classes.drawerClose]: !open,
+          }),
+        }}
+      >
+        <div className={styles.toolbar}>
+          {/* <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+          </IconButton> */}
+          <div className={`${styles.logo} ${open ? styles.logoOpen : ''}`}>
+            <img src="/images/logo-48px.png" />
+            <span>La Danze en LDC</span>
+          </div>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={toggleDrawer}
+            edge="start"
+            className={`${styles.toggleButton} ${open ? styles.open : ''}`}
+          >
+            <ChevronRightOutlinedIcon />
+          </IconButton>
+        </div>
+        {/* <Divider /> */}
+        <List>
+          {['Mon compte', 'Paramètres'].map((text, index) => (
+            <ListItem selected={index % 2 === 0} button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <AccountCircleOutlinedIcon /> : <SettingsOutlinedIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Typography paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+          facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+          gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+          donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+          Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
+          imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
+          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+          donec massa sapien faucibus et molestie ac.
+        </Typography>
+        <Typography paragraph>
+          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
+          facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
+          tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
+          consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
+          vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
+          hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
+          tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
+          nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
+          accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
+        </Typography>
+      </main>
+    </div>
+  );
+}
